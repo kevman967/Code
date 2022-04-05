3870e8e02b378f8f38c5c267a4b2972d3cd85a33
kevman967 <kevman966@gmail.com>
2022-04-05 17:25:33 -0400

    master

--- pointers.cpp
+++ pointers.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 using namespace std;
+void compute(int*);
 int main(){
     int a =5;
-    int &b = a;
-    cout<< b;
+    int *ptr;//declare a pointer
+    ptr = nullptr; // initialize pointer as nullptr
+    ptr = &a; // assign memory adress of a to the pointer
+    cout << "ptr has been the assigned the adress of variable a which is: "<< ptr << endl;
+    compute(&a);
+    cout<< "changed a with a function. The new data is: "<< *ptr << endl;
+    cout<< "This number shows the size of the memory allocated for a which is: "<< sizeof(*ptr);
+}
+void compute(int *ptr)// dont need to return value because i changed the memory adress which i can already access
+{
+    *ptr = 56;
 }
